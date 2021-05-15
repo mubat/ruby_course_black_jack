57d8c54b6f6e851dabf3cdab5ed08f126378673e
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-05-15 23:34:37 +0300

    add method to allow Player calculate points amount

--- models/persons/person.rb
+++ models/persons/person.rb
@@ -1,9 +1,16 @@
 
 class Person
+  attr_accessor :points_amount
   attr_reader :cards
 
+  def initialize
+    @cards = []
+    @points_amount = 0
+  end
+
   def calculate_points
-    
+    @cards.each { |card| @points_amount += card.points }
+    @points_amount
   end
 
   def take_cards(cards)
