2d95a9ecab6c9786687be37700cb397c47bf2ab2
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-05-16 00:35:05 +0300

    add option `reveal` to save Player's reveal state

--- models/persons/player.rb
+++ models/persons/player.rb
@@ -3,8 +3,14 @@ require_relative 'person'
 
 class Player < Person
   attr_accessor :name :cards
+  attr_writer :reveal
 
   def initialize(name)
     @name = name
+    @reveal = false
+  end
+
+  def reveal? 
+    @reveal
   end
 end
