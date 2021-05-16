f77b0cfcc01150cd90cbabb56d23c054a02df234
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-05-16 19:01:02 +0300

    rubocop: add ControllerBasic class rdoc

--- controllers/сontroller_basic.rb
+++ controllers/сontroller_basic.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+##
+# Main class for all controllers. 
+# All other controllers should implements it
 class ControllerBasic
   def confirm(message = 'Подтвердите')
     ['y', 'д', '+'].include?(prompt("#{message} y/д/+ - да").downcase)
